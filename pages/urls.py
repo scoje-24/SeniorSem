8684d3bba2a01a888225ca7567588bfee7056f5a
Kiera Lesky <leskj-24@rhodes.edu>
2024-03-28 12:18:52 -0500

    HTML5 integration with Django

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,16 @@ from pages import views
 
 urlpatterns = [
     path("", views.home, name='home'),
-    path("about", views.about, name='about'),
+    path("about.html", views.about, name='about'),
+    path("botanical.html", views.botanical, name='botanical'),
+    path("brooks.html", views.brooks, name='brooks'),
+    path("cmom.html", views.cmom, name='cmom'),
+    path("crosstown.html", views.crosstown, name='crosstown'),
+    path("exhibits.html", views.exhibits, name='exhibits'),
+    path("metal.html", views.metal, name='metal'),
+    path("mosh.html", views.mosh, name='mosh'),
+    path("ncrm.html", views.ncrm, name='ncrm'),
+    path("rocknsoul.html", views.rocknsoul, name='rocknsoul'),
+    path("stax.html", views.stax, name='stax'),
+    path("sun.html", views.sun, name='sun'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,36 @@ def home(request):
 
 def about(request):
     return render(request, "pages/about.html", {})
+
+def exhibits(request):
+    return render(request, "pages/exhibits.html", {})
+
+def botanical(request):
+    return render(request, "pages/botanical.html", {})
+
+def brooks(request):
+    return render(request, "pages/brooks.html", {})
+
+def cmom(request):
+    return render(request, "pages/cmom.html", {})
+
+def crosstown(request):
+    return render(request, "pages/crosstown.html", {})
+
+def metal(request):
+    return render(request, "pages/metal.html", {})
+
+def mosh(request):
+    return render(request, "pages/mosh.html", {})
+
+def ncrm(request):
+    return render(request, "pages/ncrm.html", {})
+
+def rocknsoul(request):
+    return render(request, "pages/rocknsoul.html", {})
+
+def stax(request):
+    return render(request, "pages/stax.html", {})
+
+def sun(request):
+    return render(request, "pages/sun.html", {})
